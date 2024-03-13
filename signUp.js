2ac67f048955d027f1937652bfca68d4c9297f5d
jihye <kwisdom95@naver.com>
2024-03-13 14:16:08 +0900

    js파일 추가

---
+++ signUp.js
@@ -0,0 +1,9 @@
+let Id=document.querySelector(".id");
+let Password=document.querySelector(".password");
+let Password2=document.querySelector(".password2");
+let Email=document.querySelector(".email");
+let Nickname=document.querySelector(".name");
+
+if(Password.value !== Password2.value){
+    
+}
