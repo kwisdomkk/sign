b67dfc8a3b484783701f5a54cdb74cecfb88f7be
jihye <kwisdom95@naver.com>
2024-03-14 16:16:29 +0900

    error 수정중1

--- signIn.js
+++ signIn.js
@@ -2,24 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
   const signInForm = document.querySelector(".sign-in-form");
   const idInput = document.querySelector(".id");
   const passwordInput = document.querySelector(".password");
-  const idError = document.querySelector(".id_error");
-  const pwError = document.querySelector(".pw_error");
+  const idError = document.querySelector(".id-error");
+  const pwError = document.querySelector(".pw-error");
 
   signInForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    if (idInput.value === "") {
-      idError.style.display = "block";
-      idInput.focus();
-    } else {
-      idError.style.display = "none";
-    }
-
-    if (passwordInput.value === "") {
-      pwError.style.display = "block";
-      passwordInput.focus();
-    } else {
-      pwError.style.display = "none";
+    if(idInput.value===""){
+      idError.style.display="block";
+    } if(idInput.value !== ""&&passwordInput.value===""){
+      idError.style.display="none";
+      pwError.style.display="block";
     }
   });
 });
